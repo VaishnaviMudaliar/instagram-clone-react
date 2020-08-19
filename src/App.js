280b2d433a3fd529a42617d51f69b6ca7a16297f
VMudaliar <vaishnavim1311@gmail.com>
2020-08-19 16:16:26 +0530

    commit on master

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Post from './Post'
+
+function App() {
+  return (
+    <div className="App">
+      
+      <div className="App_header">
+        <img className="App_header_image"
+         src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"></img>
+      </div>
+
+      </Post>
+    </div>
+  );
+}
+
+export default App;
